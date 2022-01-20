e052ab75853bc54d449ceeda600d14a6845a144e
leedd87 <leedav87@gmail.com>
2022-01-20 15:59:26 +0900

    Initial Commit

---
+++ Shared/swiftGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftGitHubApp.swift
+//  Shared
+//
+//  Created by mac-lee on 20/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
